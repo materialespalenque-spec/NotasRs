3acdfae9e9d2b888bfa5d1ea906e9c8995e45bb8
materialespalenque-spec <materialespalenque@gmail.com>
2026-07-10 19:30:45 -0600

    Update firebase-config.js
    correjido

--- firebase-config.js
+++ firebase-config.js
@@ -4,10 +4,8 @@ const firebaseConfig = {
   projectId: "notasrapidasrs",
   storageBucket: "notasrapidasrs.firebasestorage.app",
   messagingSenderId: "218766427490",
-  appId: "1:218766427490:web:7a0f4e8b70fb197bfd183b",
-  measurementId: "G-JQE5WZJ3KL"
+  appId: "1:218766427490:web:7a0f4e8b70fb197bfd183b"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
